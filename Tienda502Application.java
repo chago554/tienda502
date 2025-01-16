33244cd9ab3cbd262717a15ab3695058ebf995d6
Santiago <santiagojesuslaureano369@gmail.com>
2025-01-16 11:14:13 -0600

    Se pone el Bean para permitir la conexion

--- Tienda502Application.java
+++ Tienda502Application.java
@@ -2,6 +2,9 @@ package com.utsem.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Tienda502Application {
@@ -9,5 +12,18 @@ public class Tienda502Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Tienda502Application.class, args);
 	}
-
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500","http://localhost")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+	
 }
