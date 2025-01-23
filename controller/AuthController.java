9af2f71d921b7ca2b3b8eac630ac7868d65a8918
Santiago <santiagojesuslaureano369@gmail.com>
2025-01-23 09:09:20 -0600

    Se crea el controller para el auth

---
+++ controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.utsem.app.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.utsem.app.dto.EstatusUsuarioDTO;
+import com.utsem.app.dto.LoginDTO;
+import com.utsem.app.service.UsuarioService;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+	@Autowired
+	UsuarioService usuarioService;
+	//login
+	@PostMapping("login")
+	public EstatusUsuarioDTO postLogin(@RequestBody LoginDTO datos, HttpSession sesion) {
+		System.out.println(sesion.getId());
+		return usuarioService.login(datos, sesion);
+	}
+	
+	//estatus
+	@PostMapping("estatus")
+	public EstatusUsuarioDTO postEstatus(HttpSession sesion) {
+		return sesion.getAttribute("estatusUsuario") != null ? (EstatusUsuarioDTO)sesion.getAttribute("estatusUsuario") : new EstatusUsuarioDTO();
+	}
+	
+	//logout
+	@PostMapping("logout")
+	public EstatusUsuarioDTO posLogout(HttpSession sesion) {
+		sesion.invalidate();
+		return new EstatusUsuarioDTO("Sesion finalizada");
+	}
+	
+	
+	
+}
