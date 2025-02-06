2909eb1594e42f8424a8b882f7d6b2919bf920f3
chago554 <santiagojesuslaureano369@gmail.com>
2025-02-05 20:02:00 -0600

    Corrigiendo

--- util/ModelMapperConfig.java
+++ util/ModelMapperConfig.java
@@ -1,7 +1,15 @@
 //Este paquete se usa para poner todas las utils que se utilizan en el proyecto
 package com.utsem.app.util;
 
+<<<<<<< HEAD
 import org.modelmapper.ModelMapper;
+=======
+<<<<<<< HEAD
+import org.modelmapper.ModelMapper;
+=======
+import org.modelmapper.ModelMapper ;
+>>>>>>> 4e75c0a (funciona)
+>>>>>>> a64e00a (Corrigiendo)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
